186f32c2724eaa5298761cd4fb7c62f6b94270cc
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-07-25 07:44:59 +0530

    feat: add func for h1 and p

--- hello.hs
+++ hello.hs
@@ -14,9 +14,15 @@ head_ = el "head"
 title_ :: String -> String
 title_ = el "title"
 
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 main = putStrLn myhtml
 
-myhtml = makeHtml "My page title" "My page content"
+myhtml = makeHtml "My page title" (h1_ "Welcome" <> p_ "My first paragraph") 

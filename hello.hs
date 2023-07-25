eb5c87216c5bba971a90b485d2c0568dbab90769
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-07-25 08:26:53 +0530

    refactor: add types for all functions

--- hello.hs
+++ hello.hs
@@ -1,8 +1,14 @@
 newtype Html = Html String
 newtype Structure = Structure String
+type Title = String
 
 append_ :: Structure -> Structure -> Structure
 append_ (Structure a) (Structure b) = Structure ( a <> b ) 
+
+getStructureString :: Structure -> String
+getStructureString content = 
+  case content of
+    Structure str -> str
 
 render :: Html -> String
 render html =
@@ -13,27 +19,34 @@ el:: String -> String -> String
 el tag content =
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
-html_ :: String -> String
-html_ = el "html"
+html_ :: Title -> Structure -> Html
+html_ title content =
+  Html
+    ( el "html"
+      (
+        el "head" (el "title" title)
+        <>
+        el "body" (getStructureString content)
+      )
+    )
 
-body_ :: String -> String
-body_ = el "body"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-head_ :: String -> String
-head_ = el "head"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
-title_ :: String -> String
-title_ = el "title"
+main = putStrLn (render myhtml)
 
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
-
-makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
-
-main = putStrLn myhtml
-
-myhtml = makeHtml "My page title" (h1_ "Welcome" <> p_ "My first paragraph") 
+myhtml = 
+  html_
+    "Page title"
+    (
+      append_
+        (h1_ "Heading")
+        (
+          append_
+            (p_ "Paragraph #1")
+            (p_ "Paragraph #2")
+        )
+    )

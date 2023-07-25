283333d3b60ebdf39ebbefbc5f4cb33951ad2d95
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-07-25 07:40:20 +0530

    refactor: create el func and rewrite html, head, body, title funcs using partial func

--- hello.hs
+++ hello.hs
@@ -1,14 +1,18 @@
+el:: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
 html_ :: String -> String
-html_ body = "<html>" <> body <> "</html>"
+html_ = el "html"
 
 body_ :: String -> String
-body_ content = "<body>" <> content <> "</body>"
+body_ = el "body"
 
 head_ :: String -> String
-head_ content = "<head>" <> content <> "</head>"
+head_ = el "head"
 
 title_ :: String -> String
-title_ title = "<title>" <> title <> "</title>"
+title_ = el "title"
 
 makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)

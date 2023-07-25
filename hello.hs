682e370d95ebfe6944bdc66df0437ece24b9de89
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-07-25 07:36:43 +0530

    refactor: add types for all func

--- hello.hs
+++ hello.hs
@@ -1,8 +1,16 @@
+html_ :: String -> String
 html_ body = "<html>" <> body <> "</html>"
+
+body_ :: String -> String
 body_ content = "<body>" <> content <> "</body>"
+
+head_ :: String -> String
 head_ content = "<head>" <> content <> "</head>"
+
+title_ :: String -> String
 title_ title = "<title>" <> title <> "</title>"
 
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
 main = putStrLn myhtml

b5dc4a0d04933edd17554ba63a7201861a4908f2
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-07-25 07:24:40 +0530

    feat: create seperate func for html, head, body, head wrappping

--- hello.hs
+++ hello.hs
@@ -1,4 +1,9 @@
+html_ body = "<html>" <> body <> "</html>"
+body_ content = "<body>" <> content <> "</body>"
+head_ content = "<head>" <> content <> "</head>"
+title_ title = "<title>" <> title <> "</title>"
+
 wrapHtml content = "<html><body>" <> content <> "</body></html>"
-main = putStrLn (wrapHtml myhtml)
+main = putStrLn (html_ (body_ myhtml))
 
 myhtml = "Hello World"

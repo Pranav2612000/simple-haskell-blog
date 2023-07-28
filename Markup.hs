464d5117ee9488d5f152af48767a81092fd0fcfb
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-07-28 07:41:58 +0530

    feat: support markdown parsing for h1 and ul

--- Markup.hs
+++ Markup.hs
@@ -27,6 +27,17 @@ parseLines :: Maybe Structure -> [String] -> Document
 parseLines context txts =
   case txts of
     [] -> maybeToList context
+    -- Heading 1 case
+    ('*' : ' ' : line) : rest ->
+      maybe id (:) context (Heading 1 (trim line) : parseLines Nothing rest)
+    -- Unordered list case
+    ('-' : ' ' : line) : rest ->
+      case context of
+        Just (UnorderedList list) ->
+          parseLines (Just (UnorderedList ( list <> [trim line]))) rest
+        _ ->
+          maybe id (:) context (parseLines (Just (UnorderedList [trim line] )) rest)
+    -- Paragraph case
     currentLine : rest ->
       let
         line = trim currentLine

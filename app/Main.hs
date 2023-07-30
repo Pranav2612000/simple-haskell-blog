42ca518a8fd47ba8447b1ad5ea4219dc706a4fee
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-07-30 17:19:01 +0530

    feat: create ADT for parsing input

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,101 @@
 module Main where
 
 import qualified HsBlog
+import Options.Applicative
+import Data.Maybe (fromMaybe)
+
+data Options
+  = ConvertSingle SingleInput SingleOutput
+  | ConvertDir FilePath FilePath
+  deriving Show
+
+data SingleInput
+  = Stdin
+  | InputFile FilePath
+  deriving Show
+
+data SingleOutput
+  = Stdout
+  | OutputFile FilePath
+  deriving Show
+
+inp :: Parser FilePath
+inp = 
+  strOption
+    ( long "input"
+      <> short 'i'
+      <> metavar "FILE"
+      <> help "Input file"
+    )
+
+out :: Parser FilePath
+out =
+  strOption
+    ( long "output"
+      <> short 'o'
+      <> metavar "FILE"
+      <> help "Output file"
+    )
+
+mapMaybe :: (a -> b) -> Maybe a -> Maybe b
+mapMaybe func maybeX =
+  case maybeX of
+    Nothing -> Nothing
+    Just x -> Just (func x)
+
+pSingleInput :: Parser SingleInput
+pSingleInput = fromMaybe Stdin <$> optional pInputFile
+
+pSingleOutput :: Parser SingleOutput
+pSingleOutput = fromMaybe Stdout <$> optional pOutputFile
+
+pInputFile :: Parser SingleInput
+pInputFile = fmap InputFile parser
+  where
+    parser =
+      strOption
+        ( long "input"
+          <> short 'i'
+          <> metavar "FILE"
+          <> help "Input file"
+        )
+
+pOutputFile :: Parser SingleOutput
+pOutputFile = fmap OutputFile parser
+  where
+    parser =
+      strOption
+        ( long "output"
+          <> short 'i'
+          <> metavar "FILE"
+          <> help "Output file"
+        )
+
+pConvertSingle :: Parser Options
+pConvertSingle =
+  ConvertSingle <$> pSingleInput <*> pSingleOutput
+
+pInputDir :: Parser FilePath
+pInputDir = 
+  strOption
+    ( long "input"
+      <> short 'i'
+      <> metavar "DIRECTORY"
+      <> help "Input directory"
+    )
+
+pOutputDir :: Parser FilePath
+pOutputDir =
+  strOption
+    ( long "output"
+      <> short 'o'
+      <> metavar "DIRECTORY"
+      <> help "Output directory"
+    )
+
+pConvertDir :: Parser Options
+pConvertDir =
+  ConvertDir <$> pInputDir <*> pOutputDir
 
 main :: IO()
 main = HsBlog.main

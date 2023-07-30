d32bdb74d4475cee2e5225124e430fb66d682add
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-07-30 14:47:02 +0530

    feat: update main func to take input & output filenames as cmd line args

--- hello.hs
+++ hello.hs
@@ -12,6 +12,19 @@ main =
       [] ->
         getContents >>= \content ->
           putStrLn (process "Empty title" content)
+      -- With input and output file paths as program arguments
+      [inputFile, outputFile] ->
+        readFile inputFile >>= \content ->
+          doesFileExist outputFile >>= \fileExists ->
+            case fileExists of
+              False ->
+                writeFile outputFile (process inputFile content)
+              True ->
+                putStrLn "Output file already exists. Overwrite ( Y/N )" *>
+                  getLine >>= \answer ->
+                    case answer of
+                      "Y" ->
+                        writeFile outputFile (process inputFile content)
       _ ->
         putStrLn "Usage: runghc Main.hs [ -- <input-file> <output-file>]"
 

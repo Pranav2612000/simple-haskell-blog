5781c4f5e8bc6b4a395277280faa3c767616e582
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-07-31 08:23:26 +0530

    feat: create func for building index page

--- src/HsBlog.hs
+++ src/HsBlog.hs
@@ -5,6 +5,7 @@ module HsBlog
   where
 
 import HsBlog.Html as Html
+import HsBlog.Markup as Markup
 import HsBlog.Convert
 
 import System.IO
@@ -16,3 +17,26 @@ convertSingle title input output = do
 
 convertDir :: FilePath -> FilePath -> IO ()
 convertDir = error "Not implemented"
+
+buildIndex :: [(FilePath, Markup.Document)] -> Html.Html
+buildIndex files =
+  let
+    content =
+      map
+        ( \(file, doc) ->
+          case doc of
+            Markup.Heading 1 heading : article ->
+              Html.h_ 3 ( Html.link_ file (Html.txt_ heading) )
+                <> foldMap convertStructure ( take 3 article )
+                <> Html.p_ ( Html.link_ file (Html.txt_ "..."))
+            _ ->
+              Html.h_ 3 (Html.link_ file (Html.txt_ file))
+        )
+        files
+  in
+    Html.html_
+      "Home" 
+      ( Html.h_ 1 (Html.link_ "index.html" ( Html.txt_ "Blog" ) )
+        <> Html.h_ 2 ( Html.txt_ "Posts" )
+        <> mconcat content 
+      )

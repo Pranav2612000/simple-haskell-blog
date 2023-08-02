5c8eda3eb6109a1ee897460711c45cd2b4e50631
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-08-02 08:17:44 +0530

    feat: create outline for fn convert directory fn with some helper fns

---
+++ src/HsBlog/Directory.hs
@@ -0,0 +1,73 @@
+module HsBlog.Directory
+  ( convertDirectory
+  )
+where
+
+import qualified HsBlog.Markup as Markup
+import qualified HsBlog.Html as Html
+import HsBlog.Convert (convert, convertStructure)
+
+import Data.List ( partition )
+import Data.Traversable (for)
+import Control.Monad (void, when)
+
+import System.IO (hPutStrLn, stderr)
+import Control.Exception (catch, displayException, SomeException(..))
+import System.Exit (exitFailure)
+import System.FilePath
+  ( takeExtension
+  , takeBaseName
+  , (<.>)
+  , (</>)
+  , takeFileName
+  )
+import System.Directory
+  ( createDirectory
+  , removeDirectoryRecursive
+  , listDirectory
+  , doesDirectoryExist
+  , copyFile
+  )
+
+convertDirectory :: FilePath -> FilePath -> IO()
+convertDirectory inputDir outputDir = do
+  DirContents filesToProcess filesToCopy <- getDirFilesAndContent inputDir
+  createOutputDirectoryOrExit outputDir
+  let
+    outputHtmls = txtsToRenderedHtml filesToProcess
+  copyFiles outputDir filesToCopy
+  writeFiles outputDir outputHtmls
+  putStrLn "Done."
+
+data DirContents =
+  DirContents
+  { dcFilesToProcess :: [(FilePath, String)]
+  , dcFilesToCopy :: [FilePath]
+  }
+
+getDirFilesAndContent :: FilePath -> IO DirContents
+getDirFilesAndContent inputDir = do
+  files <- map (inputDir </>) <$> listDirectory inputDir
+  let
+    (txtFiles, otherFiles) =
+      partition ((== ".txt") . takeExtension) files
+  txtFilesAndContent <-
+    applyIoOnList readFile txtFiles >>= filterAndReportFailures
+  pure $ DirContents
+    { dcFilesToProcess = txtFilesAndContent
+    , dcFilesToCopy = otherFiles
+    }
+
+applyIoOnList :: (a -> IO b) -> [a] -> IO [(a, Either String b)]
+applyIoOnList fn list =
+  traverse
+    (\file -> do
+      maybeResult <-
+        catch
+          (Right <$> fn file)
+          ( \(SomeException e) -> do
+            pure $ Left (displayException e)
+          )
+      pure (file, maybeResult)
+    )
+    list

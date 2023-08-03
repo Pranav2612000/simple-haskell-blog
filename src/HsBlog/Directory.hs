174d7a22847a2a24b0c3d40dfdd010c059d6bf4f
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-08-03 08:06:26 +0530

    refactor: move buildIndex func to Directory.hs

--- src/HsBlog/Directory.hs
+++ src/HsBlog/Directory.hs
@@ -5,7 +5,7 @@ where
 
 import qualified HsBlog.Markup as Markup
 import qualified HsBlog.Html as Html
-import HsBlog.Convert (process, convert, convertStructure)
+import HsBlog.Convert (convert, convertStructure)
 
 import Data.List ( partition )
 import Data.Traversable (for)
@@ -125,6 +125,29 @@ toOutputMarkupFile (file, content) =
 
 convertFile :: (FilePath, Markup.Document) -> (FilePath, Html.Html)
 convertFile (file, doc) = (file, convert file doc)
+
+buildIndex :: [(FilePath, Markup.Document)] -> Html.Html
+buildIndex files =
+  let
+    content =
+      map
+        ( \(file, doc) ->
+          case doc of
+            Markup.Heading 1 heading : article ->
+              Html.h_ 3 ( Html.link_ file (Html.txt_ heading) )
+                <> foldMap convertStructure ( take 3 article )
+                <> Html.p_ ( Html.link_ file (Html.txt_ "..."))
+            _ ->
+              Html.h_ 3 (Html.link_ file (Html.txt_ file))
+        )
+        files
+  in
+    Html.html_
+      "Home" 
+      ( Html.h_ 1 (Html.link_ "index.html" ( Html.txt_ "Blog" ) )
+        <> Html.h_ 2 ( Html.txt_ "Posts" )
+        <> mconcat content 
+      )
 
 confirm :: String -> IO Bool
 confirm question = do

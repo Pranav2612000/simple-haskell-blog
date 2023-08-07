5f52f9b0036f1ea0432eba0f26e82efcddca9991
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-08-07 08:08:26 +0530

    test: add multiline p and ol test for markup parsing

--- test/MarkupParsingSpec.hs
+++ test/MarkupParsingSpec.hs
@@ -1,7 +1,28 @@
+{-# language QuasiQuotes #-}
+
 module MarkupParsingSpec where
 
 import Test.Hspec
+import Text.RawString.QQ
 import HsBlog.Markup
+
+example3 :: String
+example3 = [r|
+Remember that multiple lines with no seperation are grouped together
+into a single paragraph but list items remains seperate
+
+# Item 1
+# Item 2
+|]
+
+example3Result :: Document
+example3Result = 
+  [ Paragraph "Remember that multiple lines with no seperation are grouped together into a single paragraph but list items remains seperate"
+  , OrderedList
+    [ "Item 1"
+    , "Item 2"
+    ]
+  ]
 
 spec :: Spec
 spec = do
@@ -30,5 +51,13 @@ simple = do
       shouldBe
         (parse "> main = putStrLn \"hello world\"")
         [CodeBlock ["main = putStrLn \"hello world\""]]
+
+multiline :: Spec
+multiline = do
+  describe "Multi-line tests" $ do
+    it "example3" $ do
+      shouldBe
+        (parse example3)
+        example3Result
 
 
